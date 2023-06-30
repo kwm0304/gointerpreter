44598da20b1701576ea64860f3660bc210cb81c3
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-06-30 15:43:40 -0400

    starting 2.3

---
+++ 1/parser/parser.go
@@ -0,0 +1,4 @@
+/* parser is a software component that takes input and builds a data structure, often some kind of parse tree like js object. lexer feeds tokens into parser 
+this is going to be a Pratt parser(top-down operator precedence parser) which is a type of recursive descent parser.
+*/
+
