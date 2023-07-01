01d4b00c55ec968abe0a4fd74129607620cfbe46
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-07-01 14:31:57 -0400

    added parser test

--- 1/ast/ast.go
+++ 1/ast/ast.go
@@ -37,7 +37,7 @@ type LetStatement struct{
 	Value Expression
 }
 
-func (ls *LetStatement) statementNode()
+func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal}
 
 type Identifier struct {
