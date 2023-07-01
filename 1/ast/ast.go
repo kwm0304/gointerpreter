2a9c41777c4725fc2fab1840ab9505b4c6133ac2
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-06-30 23:20:31 -0400

    started on AST and parser

---
+++ 1/ast/ast.go
@@ -0,0 +1,49 @@
+package ast
+
+import "monkey/token"
+
+//every node has to implement the Node interface(has to provide TokenLiteral)
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+//will be root node of every AST produced
+//method tracks 3 fields: var name(identifier), expression that produes the value, and token
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct{
+	Token token.Token // the token.LET token
+	Name *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal}
+
+type Identifier struct {
+	Token token.Token //the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode()		{}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal}
--- 1/parser/parser.go
+++ 1/parser/parser.go
@@ -1,4 +1,41 @@
 /* parser is a software component that takes input and builds a data structure, often some kind of parse tree like js object. lexer feeds tokens into parser 
 this is going to be a Pratt parser(top-down operator precedence parser) which is a type of recursive descent parser.
+bindings in this language are in the for of let, where the <identifier> = <expression> expressions produce values, statements don't
 */
 
+package parser
+
+import (
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+
+//parser has three fields: l, curToken and peekToken
+//l is a pointer to an instance of a lexer where NextToken() is repeatedly called
+type Parser struct {
+	l *lexer.Lexer
+	
+	curToken token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	//read two tokens, so curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
